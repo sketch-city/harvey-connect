3a2bdd32934bbea9068e3f4fa437d17bcc30363d
Terry Lewis <terrylewis@outlook.com>
2017-09-08 10:22:12 -0500

    Generate UUID on launch

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { MainView } from './components/MainView';
+import { UUIDHelper } from './API/UUIDHelper'
 
 export default function AppContainer() {
+    UUIDHelper.setupUUIDIfNeeded().then((value) => {
+        console.log(`created uuid: ${value}`)
+    }).catch((error) => {
+        console.log('got error generating uuid')
+    })
     return (
         <MainView />
     );
