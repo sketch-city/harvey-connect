eb0547b0d0f2091bfbaa901f0be12fbbeceddb3f
Chris Mason <chris@chaione.com>
2020-04-12 08:51:58 -0500

    Hindi translations added
    Courtesy of Susmitha Anandarao

--- src/localization/Strings.ts
+++ src/localization/Strings.ts
@@ -1,10 +1,12 @@
 import LocalizedStrings from 'react-native-localization';
 import { en_US } from './en_US';
 import { es } from './es';
+import { hi } from './hi';
 
 
 export const strings = new LocalizedStrings({
     "en-US": en_US,
     "en": en_US,
-    "es": es
+    "es": es,
+    "hi": hi
 });
